c285c23fc6496c39f4641ab056a6cb9603aa4a6d
bluelife85 <bluelife85@naver.com>
2022-12-10 22:52:06 +0900

    added blank file

---
+++ NMEA.c
@@ -0,0 +1,9 @@
+/**
+ * \file NMEA.c
+ * \author bluelife85 (bluelife85@naver.com)
+ * \brief 
+ * \version 0.1
+ * \date 2022-12-10
+ * 
+ * 
+ */
---
+++ NMEA_types.h
@@ -0,0 +1,12 @@
+/**
+ * \file NMEA_types.h
+ * \author bluelife85 (bluelife85@naver.com)
+ * \brief 
+ * \version 0.1
+ * \date 2022-12-10
+ */
+
+#ifndef __NMEA_DATA_TYPES_H__
+#define __NMEA_DATA_TYPES_H__
+
+#endif /* __NMEA_DATA_TYPES_H__ */
